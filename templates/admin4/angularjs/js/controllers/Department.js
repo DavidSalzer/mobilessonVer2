e9476e463bd3bc8e08657d3e8f85fd239f01cebd
shaigiv <shai.givati@gmail.com>
2015-01-28 14:56:39 +0200

    - dataTables

--- templates/admin4/angularjs/js/controllers/Department.js
+++ templates/admin4/angularjs/js/controllers/Department.js
@@ -1,27 +1,7 @@
 MetronicApp.controller('department', ['$rootScope', '$scope', '$state', '$stateParams', '$http', function ($rootScope, $scope, $state, $stateParams, $http) {
     // $('#scroll-to-top').click();
 
-    /*$(function () {
-    $('#dataTable').dataTable({
-    "bJQueryUI": true,
-    "sPaginationType": "full_numbers"
-    });
-    $('#chk-all').click(function()	{
-    if($(this).is(':checked'))	{
-    $('#responsiveTable').find('.chk-row').each(function()	{
-    $(this).prop('checked', true);
-    $(this).parent().parent().parent().addClass('selected');
-    });
-    }
-    else	{
-    $('#responsiveTable').find('.chk-row').each(function()	{
-    $(this).prop('checked' , false);
-    $(this).parent().parent().parent().removeClass('selected');
-    });
-    }
-    });
-    });
-    */
+
     //$scope.departmentId = $stateParams.departmentId;
     $http.get(domain + '?action=getStatisticsByUnit&unit=department&id=' + $stateParams.departmentId)
     .success(function (data) {
@@ -39,33 +19,113 @@ MetronicApp.controller('department', ['$rootScope', '$scope', '$state', '$stateP
         console.log(data);
     })
 
-    $scope.getEmployeesByCourse = function () {
-        $http.get(domain + '?action=getStatisticsByUnit&unit=department&id=' + $stateParams.departmentId)
+    $scope.getEmployeesByCourse = function (courseId) {
+        $http.get(domain + '?action=getStatisticsByUnit&unit=employeesStatus&departmentId=' + $stateParams.departmentId + '&courseId=' + courseId)
     .success(function (data) {
+        console.log(data)
         if (data.status == 'ok') {
-            //$scope.employees = data.data;
-            $scope.employees = [{ "No": "#1001",
-                "Product": "Leather Bag",
-                "Price": "$89",
-                "Quantity": "30",
-                "Sales": "187",
-                "Date": "Oct 08,2013",
-                "Status": "In Stock"
-            },
-            { "No": "#1002",
-                "Product": "Leather Bag",
-                "Price": "$89",
-                "Quantity": "30",
-                "Sales": "187",
-                "Date": "Oct 08,2013",
-                "Status": "In Stock"
-            }]
-            console.log($scope.employees)
+            $scope.courseDetails = data.data.general;
+            $scope.employees = data.data.employees;
+
+            console.log($scope.employees);
+            initTable1();
         }
     })
     .error(function (data) {
         console.log(data);
     })
     }
+
+    var initTable1 = function () {
+        $('#employeesTable').DataTable({
+            dom: 'T<"clear">lfrtip',
+            tableTools: {
+                "sSwfPath": "../../../assets/global/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            }
+        });
+    }
+
+    //var initTable1 = function () {
+
+
+    //    var table = $('#employeesTable');
+
+    //    /* Table tools samples: https://www.datatables.net/release-datatables/extras/TableTools/ */
+
+    //    /* Set tabletools buttons and button container */
+
+    //    $.extend(true, $.fn.DataTable.TableTools.classes, {
+    //        "container": "btn-group tabletools-dropdown-on-portlet",
+    //        "buttons": {
+    //            "normal": "btn btn-sm default",
+    //            "disabled": "btn btn-sm default disabled"
+    //        },
+    //        "collection": {
+    //            "container": "DTTT_dropdown dropdown-menu tabletools-dropdown-menu"
+    //        }
+    //    });
+
+    //    var oTable = table.dataTable({
+
+    //        // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+    //        "language": {
+    //            "aria": {
+    //                "sortAscending": ": activate to sort column ascending",
+    //                "sortDescending": ": activate to sort column descending"
+    //            },
+    //            "emptyTable": "No data available in table",
+    //            "info": "Showing _START_ to _END_ of _TOTAL_ entries",
+    //            "infoEmpty": "No entries found",
+    //            "infoFiltered": "(filtered1 from _MAX_ total entries)",
+    //            "lengthMenu": "Show _MENU_ entries",
+    //            "search": "Search:",
+    //            "zeroRecords": "No matching records found"
+    //        },
+
+    //        "order": [
+    //            [0, 'asc']
+    //        ],
+    //        
+    //        "lengthMenu": [
+    //            [5, 15, 20, -1],
+    //            [5, 15, 20, "All"] // change per page values here
+    //        ],
+    //        // set the initial value
+    //        "pageLength": 10,
+
+    //        "dom": "<'row' <'col-md-12'T>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
+
+    //        // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+    //        // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+    //        // So when dropdowns used the scrollable div should be removed. 
+    //        //"dom": "<'row' <'col-md-12'T>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+    //        "tableTools": {
+    //            "sSwfPath": "../../../assets/global/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf",
+    //            "aButtons": [{
+    //                "sExtends": "pdf",
+    //                "sButtonText": "PDF"
+    //            }, {
+    //                "sExtends": "csv",
+    //                "sButtonText": "CSV"
+    //            }, {
+    //                "sExtends": "xls",
+    //                "sButtonText": "Excel"
+    //            }, {
+    //                "sExtends": "print",
+    //                "sButtonText": "Print",
+    //                "sInfo": 'Please press "CTR+P" to print or "ESC" to quit',
+    //                "sMessage": "Generated by DataTables"
+    //            }]
+    //        }
+    //    });
+
+    //    var tableWrapper = $('#employeesTable_wrapper'); // datatable creates the table wrapper by adding with id {your_table_jd}_wrapper
+
+    //          tableWrapper.find('.dataTables_length select'); // initialize select2 dropdown
+    //}
+
+    //initTable1();
+
 
 } ])
